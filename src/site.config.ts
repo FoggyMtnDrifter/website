91cb6acf596eb3cb9747c3bfd8e40717bf702706
Michael Kinder <michael@foggymtndrifter.com>
2026-01-25 22:06:30 -0500

    update configs

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://foggymtndrifter.com',
+  site: 'https://www.foggymtndrifter.com',
   // The name of your site, used in the title and for SEO.
   title: 'FoggyMtnDrifter',
   // The description of your site, used for SEO and RSS feed.
